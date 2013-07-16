4005e577c9ac9a75e6347f14c22a5e8f556d9e73
zhuruixian <zhuruixian@zhuruixian-OptiPlex-9010.(none)>
2013-07-16 11:17:50 +0800

    New Change.

---
+++ src/main/java/com/yuxin001/site/web/interceptor/CommonInterceptor.java
@@ -0,0 +1,20 @@
+package com.yuxin001.site.web.interceptor;
+
+import net.paoding.rose.web.ControllerInterceptorAdapter;
+import net.paoding.rose.web.Invocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonInterceptor extends ControllerInterceptorAdapter {
+
+	static final Logger LOG = LoggerFactory.getLogger(CommonInterceptor.class);
+
+	@Override
+	public Object before(Invocation inv) throws Exception {
+		inv.addModel("contextPath", inv.getRequest().getContextPath());
+		return true;
+	}
+}
